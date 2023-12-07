a446240456914d1da06b7ff1eb87a344bdf2ada9
prp20 <ppprasad112@gmail.com>
2023-12-07 23:06:43 +0530

    Added utility functions such as plot loss curves

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import torch
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from PIL import Image
 
 
 def save_model(model: torch.nn.Module,
@@ -15,3 +19,49 @@ def save_model(model: torch.nn.Module,
     print(f"[INFO] Saving model to: {model_save_path}")
     torch.save(obj=model.state_dict(),
                f=model_save_path)
+
+
+def plot_loss_curves(results: Dict[str, List[float]]):
+    loss = results['train_loss']
+    test_loss = results['test_loss']
+    accuracy = results['train_acc']
+    test_accuracy = results['test_acc']
+
+    epochs = range(len(results['train_loss']))
+
+    plt.figure(figsize=(15, 7))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='train_loss')
+    plt.plot(epochs, test_loss, label='test_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label='train_accuracy')
+    plt.plot(epochs, test_accuracy, label='test_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+
+def plot_transformed_images(image_paths, transform, n=3, seed=42):
+    random.seed(seed)
+    random_image_paths = random.sample(image_paths, k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(1, 2)
+            ax[0].imshow(f)
+            ax[0].set_title(f"Original \nSize: {f.size}")
+            ax[0].axis("off")
+            transformed_image = transform(f).permute(1, 2, 0)
+            ax[1].imshow(transformed_image)
+            ax[1].set_title(f"Transformed \nSize: {transformed_image.shape}")
+            ax[1].axis("off")
+
+            fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+
+
+plot_transformed_images(image_path_list,
+                        transform=data_transform,
+                        n=3)
